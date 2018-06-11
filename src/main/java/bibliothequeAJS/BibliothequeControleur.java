7148a854ff256ed535eec25f482e8accd485674e
XyoloJR <xyolo@msn.com>
2018-06-11 12:09:03 +0200

    Application et bibliothèque controller

--- src/main/java/bibliothequeAJS/BibliothequeControleur.java
+++ src/main/java/bibliothequeAJS/BibliothequeControleur.java
@@ -1,5 +1,19 @@
 package bibliothequeAJS;
 
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BibliothequeControleur {
+
+  private List<Livre> livres;
+
+  @GetMapping(path = "/livres", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public List<Livre> getLivres() {
+    return livres;
+  }
 
 }
