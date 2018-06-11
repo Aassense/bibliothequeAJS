9ef266d957c8e79f365ae9ac99276cc6a9944407
Allan <allan.assense@hotmail.fr>
2018-06-11 14:58:38 +0200

    Service teste

--- src/main/java/bibliothequeAJS/BibliothequeControleur.java
+++ src/main/java/bibliothequeAJS/BibliothequeControleur.java
@@ -1,38 +1,58 @@
 package bibliothequeAJS;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/bibliothequeAJS")
 public class BibliothequeControleur {
 
-  private List<Livre> livres;
+	private List<Livre> livres;
+	private Transfert transfert = new Transfert();
 
-  @GetMapping(path = "/livres", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public List<Livre> getLivres() {
-    updateLivres();
-    return livres;
-  }
+	@GetMapping(path = "/livres", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public List<Livre> getLivres() {
+		updateLivres();
+		return livres;
+	}
 
-  @GetMapping(path = "/livres/{index}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public Livre getOccupation(int index) {
-    updateLivres();
-    for (Livre livre : livres) {
-      if (livre.getId() == index) {
-        return livre;
-      }
-    }
-    return null;
-  }
+	@GetMapping(path = "/livres/{index}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 
-  // met à jour la liste des livres depuis la base de données
-  private void updateLivres() {
-    livres = new ArrayList<Livre>();
-    // TODO appel à la base
-  }
+	public Livre getOccupation(@PathVariable("index") int index) {
+
+		updateLivres();
+		for (Livre livre : livres) {
+			if (livre.getId() == index) {
+				return livre;
+			}
+		}
+		return null;
+	}
+
+	private void updateLivres() {
+		livres = transfert.getLivres();
+
+	}
+
+	@PostMapping(path = "/transfert/{index}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	private String deleteLivre(@PathVariable("index") int index) {
+		switch (transfert.deleteLivre(index)) {
+		case Transfert.ERREUR_SQL:
+			return "Probleme de base de donnee";
+
+		case Transfert.LIVRE_ABSENT:
+			return "Le livre n'existe pas";
+
+		default:
+			return "Retrait reussi";
+		}
+
+	}
 
 }
--- src/main/java/bibliothequeAJS/Transfert.java
+++ src/main/java/bibliothequeAJS/Transfert.java
@@ -10,38 +10,44 @@ import java.util.ArrayList;
 
 public class Transfert {
 	private Connection connection = null;
-	private final static String URL = "jdbc:mysql://192.168.1.28/bibliothequeAJS";
-	private final static String login = "root";
-	private final static String password = "";
+	private final static String URL = "jdbc:mysql://192.168.1.28/bibliotheque";
+	private final static String login = "user";
+	private final static String password = "user";
 
 	private final static String GetLivresRequest = "SELECT * " + "FROM livre";
 	private final static String DeleteLivreRequest = "DELETE FROM livre WHERE id =? ";
+	private final static String ExisteLivreRequest = "SELECT COUNT(id) FROM livre WHERE id=?";
+	public final static int LIVRE_ABSENT = 1;
+	public final static int LIVRE_EFFACE = 2;
+	public final static int ERREUR_SQL = 3;
 
-	public void deleteLivre(int id) {
+	public int deleteLivre(int id) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			// La connexion
+
 			connection = DriverManager.getConnection(URL, login, password);
 			System.out.println("connexion à la base réussie");
-			PreparedStatement stmtDelete = connection.prepareStatement(DeleteLivreRequest);
-			stmtDelete.setInt(1, id);
+			PreparedStatement stmtExiste = connection.prepareStatement(ExisteLivreRequest);
+			stmtExiste.setInt(1, id);
+			ResultSet resultSet = stmtExiste.executeQuery();
 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			// On ferme la connexion
-			if (connection != null) {
-				try {
-					connection.close();
-				} catch (final SQLException e) {
-					e.printStackTrace();
-				}
+			if (resultSet.next() && resultSet.getInt(1) == 0) {
+				connection.close();
+				return LIVRE_ABSENT;
+
+			} else {
+
+				PreparedStatement stmtDelete = connection.prepareStatement(DeleteLivreRequest);
+				stmtDelete.setInt(1, id);
+				stmtDelete.executeUpdate();
+				stmtDelete.close();
+				connection.close();
+				return LIVRE_EFFACE;
 			}
 
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ERREUR_SQL;
 		}
 
 	}
@@ -50,11 +56,11 @@ public class Transfert {
 		ArrayList<Livre> list = new ArrayList<>();
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			// La connexion
+
 			connection = DriverManager.getConnection(URL, login, password);
 			System.out.println("connexion à la base réussie");
 			Statement stmtEmprunts = connection.createStatement();
-			// Remplir la requête
+
 			ResultSet rs = stmtEmprunts.executeQuery(GetLivresRequest);
 			while (rs.next()) {
 				Livre livre = new Livre(rs.getInt("id"), rs.getString("titre"), rs.getInt("annee"),
@@ -64,13 +70,13 @@ public class Transfert {
 			}
 
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		} finally {
-			// On ferme la connexion
+
 			if (connection != null) {
 				try {
 					connection.close();
