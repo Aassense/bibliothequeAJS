c01492ede6e38ecdb731f909928f849929f3d993
Allan <allan.assense@hotmail.fr>
2018-06-11 12:22:36 +0200

    Transfert

--- src/main/java/bibliothequeAJS/Transfert.java
+++ src/main/java/bibliothequeAJS/Transfert.java
@@ -1,138 +1,22 @@
 package bibliothequeAJS;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Transfert {
 	private Connection connection = null;
-	private final static String URL = "jdbc:mysql://localhost/bibliotheque";
+	private final static String URL = "jdbc:mysql://localhost/bibliothequeAJS";
 	private final static String login = "root";
 	private final static String password = "";
-	private final static String AddLivreRequest = "INSERT INTO livre (titre, annee, auteur, editeur)"
-			+ "VALUES (?,?,?,?)";
 
-	private final static String GetEmpruntsRequest = "SELECT * " + "FROM emprunt " + "WHERE date_retour IS NULL "
-			+ "ORDER BY date_emprunt DESC ";
 	private final static String GetLivresRequest = "SELECT * " + "FROM livre";
 
-	private final static String AddEmpruntRequest = "INSERT INTO emprunt (livre_id, usager, date_emprunt)"
-			+ "VALUES (?,?,?)";
-
-	private final static String UpdateEmpruntRequest = "UPDATE emprunt SET date_retour = ? WHERE id = ?";
-
-	private final static String GetLivresDispoRequest = "SELECT *"
-			+ "FROM livre WHERE id NOT IN (SELECT livre_id FROM emprunt)";
-
-	public void addLivre(Livre livre) {
-		PreparedStatement stmtLivre;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			// La connexion
-			connection = DriverManager.getConnection(URL, login, password);
-			System.out.println("connection à la base réussie");
-
-			stmtLivre = connection.prepareStatement(AddLivreRequest);
-			// Remplir la requête
-			stmtLivre.setString(1, livre.getTitre());
-			stmtLivre.setInt(2, livre.getAnnee());
-			stmtLivre.setString(3, livre.getAuteur());
-			stmtLivre.setString(4, livre.getEditeur());
-			stmtLivre.executeUpdate();
-			stmtLivre.close();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			// On ferme la connexion
-			if (connection != null) {
-				try {
-					connection.close();
-				} catch (final SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-	}
-
-	public void addEmprunt(Emprunt emprunt) {
-		PreparedStatement stmtEmprunt;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			// La connexion
-			connection = DriverManager.getConnection(URL, login, password);
-			System.out.println("connection à la base réussie");
-
-			stmtEmprunt = connection.prepareStatement(AddEmpruntRequest);
-			// Remplir la requête
-			stmtEmprunt.setInt(1, emprunt.getLivreId());
-			stmtEmprunt.setString(2, emprunt.getUsager());
-			stmtEmprunt.setDate(3, emprunt.getDateEmprunt());
-			stmtEmprunt.executeUpdate();
-			stmtEmprunt.close();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			// On ferme la connexion
-			if (connection != null) {
-				try {
-					connection.close();
-				} catch (final SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-	}
-
-	public void updateEmprunt(Emprunt emprunt, Date dateRetour) {
-		PreparedStatement stmtEmprunt;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			// La connexion
-			connection = DriverManager.getConnection(URL, login, password);
-			System.out.println("connection à la base réussie");
-
-			stmtEmprunt = connection.prepareStatement(UpdateEmpruntRequest);
-			// Remplir la requête
-			stmtEmprunt.setDate(1, dateRetour);
-			stmtEmprunt.setInt(2, emprunt.getId());
-			stmtEmprunt.executeUpdate();
-			stmtEmprunt.close();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			// On ferme la connexion
-			if (connection != null) {
-				try {
-					connection.close();
-				} catch (final SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-	}
-
 	public ArrayList<Livre> getLivres() {
-		ArrayList<Livre> list = new ArrayList<Livre>();
+		ArrayList<Livre> list = new ArrayList<>();
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			// La connexion
@@ -166,43 +50,6 @@ public class Transfert {
 
 		}
 		return list;
-	}
 
-	public ArrayList<Emprunt> getEmprunts() {
-		ArrayList<Emprunt> list = new ArrayList<Emprunt>();
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			// La connexion
-			connection = DriverManager.getConnection(URL, login, password);
-			System.out.println("connection à la base réussie");
-			Statement stmtEmprunts = connection.createStatement();
-			// Remplir la requête
-			ResultSet rs = stmtEmprunts.executeQuery(GetEmpruntsRequest);
-			while (rs.next()) {
-				Emprunt emprunt = new Emprunt(rs.getInt("id"), rs.getInt("livre_id"), rs.getString("usager"),
-						rs.getDate("date_emprunt"));
-				emprunt.setDateRetour(rs.getDate("date_retour"));
-				list.add(emprunt);
-				System.out.println();
-			}
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			// On ferme la connexion
-			if (connection != null) {
-				try {
-					connection.close();
-				} catch (final SQLException e) {
-					e.printStackTrace();
-				}
-			}
-
-		}
-		return list;
 	}
 }
