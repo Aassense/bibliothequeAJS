c3b1914ba67ecd48050e43e77f943fdde2cbdc52
Allan <allan.assense@hotmail.fr>
2018-06-11 12:04:56 +0200

    commit class livre l Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/bibliothequeAJS/Livre.java
@@ -0,0 +1,63 @@
+package bibliothequeAJS;
+
+public class Livre {
+	private int id;
+	private String titre;
+	private int annee;
+	private String auteur;
+	private String editeur;
+
+	public Livre(int id, String titre, int annee, String auteur, String editeur) {
+		this.id = id;
+		this.titre = titre;
+		this.annee = annee;
+		this.auteur = auteur;
+		this.editeur = editeur;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public int getAnnee() {
+		return annee;
+	}
+
+	public void setAnnee(int annee) {
+		this.annee = annee;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public String getEditeur() {
+		return editeur;
+	}
+
+	public void setEditeur(String editeur) {
+		this.editeur = editeur;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return id + ", " + titre + ", " + annee + ", " + auteur + ", " + editeur;
+	}
+}
